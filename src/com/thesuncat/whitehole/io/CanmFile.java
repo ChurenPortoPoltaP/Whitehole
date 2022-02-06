694ff7376e6b47038aeedc66cd70379dba0dd842
ぽーとー <churenporto@gmail.com>
2022-02-06 14:54:48 +0900

    Chinchin

--- src/com/thesuncat/whitehole/io/CanmFile.java
+++ src/com/thesuncat/whitehole/io/CanmFile.java
@@ -6,28 +6,29 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+// Chinchin
 
 public class CanmFile {
     public CanmFile(FileBase _file) throws IOException {
         file = _file;
         byteArray = _file.getContents();
         byteStream = ByteBuffer.wrap(byteArray);
-        
-        if(Settings.littleEndian)
+
+        if (Settings.littleEndian)
             byteStream.order(ByteOrder.LITTLE_ENDIAN);
-        
+
         header = new Header(byteArray);
-        for(int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++)
             keyList.add(new Keyset(i));
         currentPosition += 4;
-        
+
         byteStream.order(ByteOrder.BIG_ENDIAN);
-        for(int i = 0; i < keyList.size(); i++) {
+        for (int i = 0; i < keyList.size(); i++) {
             Keyset k = keyList.get(i);
             keyList.set(i, k.setKeyframes());
         }
     }
-    
+
     public class Header {
         public String magic = "ANDOCKAN";
         public int unk0;
@@ -49,7 +50,7 @@ public class CanmFile {
             currentPosition += 4;
         }
     }
-    
+
     public class Keyset {
         public int type; // Not part of the file.
 
@@ -75,14 +76,14 @@ public class CanmFile {
             return this;
         }
     }
-    
+
     public class Keyframe {
         public float frames;
         public float val;
         public float speed;
 
         public Keyframe(boolean isSingle) {
-            if(isSingle)
+            if (isSingle)
                 val = byteStream.getFloat(currentPosition);
             else {
                 frames = byteStream.getFloat(currentPosition);
@@ -97,50 +98,49 @@ public class CanmFile {
             speed = 0;
         }
     }
-    
+
     public void save() throws IOException {
         int fileSize = 0x20;
         fileSize += 0x60; // Keysets: there are always 8, each 0xC long
-        
+
         int keySectionSize = 0x8;
-        for(Keyset k : keyList) {
-            if(k.nKeyframes == 1)
+        for (Keyset k : keyList) {
+            if (k.nKeyframes == 1)
                 keySectionSize += 0x4;
             else
                 keySectionSize += 0xC * 12;
         }
         fileSize += keySectionSize;
         fileSize += 0x8;
-        
+
         byte[] saveArray = new byte[fileSize];
         ByteBuffer saveStream = ByteBuffer.wrap(saveArray);
-        
+
         // Header
         saveStream.put("ANDOCKAN".getBytes("UTF-8"));
-        
+
         saveStream.putInt(0x8, header.unk0);
         saveStream.putInt(0xC, header.unk1);
         saveStream.putInt(0x10, header.unk2);
         saveStream.putInt(0x14, header.unk3);
         saveStream.putInt(0x18, header.unk4);
         saveStream.putInt(0x1C, header.floatStart);
-        
+
         // Keys
         saveStream.putInt(0x80, keySectionSize);
         int curPos = 0x84;
-        for(Keyset curSet : keyList) {
+        for (Keyset curSet : keyList) {
             int f = -48;
             int nKeys = 0;
             int y = 0;
             curSet.index = (curPos - 0x84) / 4;
-            for(Keyframe k : curSet.keyframes) {
-                if(curSet.keyframes.size() == 1) {
+            for (Keyframe k : curSet.keyframes) {
+                if (curSet.keyframes.size() == 1) {
                     saveStream.putFloat(curPos, k.val);
                     nKeys = 12;
-                }
-                else {
+                } else {
                     saveStream.putFloat(curPos, f += 48);
-                    if(curSet.type == 1)
+                    if (curSet.type == 1)
                         saveStream.putFloat(curPos += 4, y += 1000);
                     else
                         saveStream.putFloat(curPos += 4, 0);
@@ -149,9 +149,9 @@ public class CanmFile {
                 curPos += 4;
                 nKeys++;
             }
-            while(nKeys < 12) {
+            while (nKeys < 12) {
                 saveStream.putFloat(curPos, f += 48);
-                if(curSet.type == 1)
+                if (curSet.type == 1)
                     saveStream.putFloat(curPos += 4, y += 1000);
                 else
                     saveStream.putFloat(curPos += 4, 0);
@@ -160,25 +160,25 @@ public class CanmFile {
                 curPos += 4;
             }
         }
-        
+
         // Keyframes
-        
+
         saveStream.position(curPos);
-        saveStream.put(new byte[] {(byte) 0x3D, (byte) 0xCC, (byte) 0xCC, (byte) 0xCD, (byte) 0x4E, (byte) 0x6E, (byte) 0x6B, (byte) 0x28, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF});
+        saveStream.put(new byte[] { (byte) 0x3D, (byte) 0xCC, (byte) 0xCC, (byte) 0xCD, (byte) 0x4E, (byte) 0x6E,
+                (byte) 0x6B, (byte) 0x28, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF });
         curPos = 0x20;
-        for(Keyset k : keyList) {
+        for (Keyset k : keyList) {
             System.out.println("Keyset " + k.type + " has " + k.keyframes.size() + " keyframes!");
-            if(k.keyframes.size() == 1)
+            if (k.keyframes.size() == 1)
                 saveStream.putInt(curPos, k.keyframes.size());
             else
                 saveStream.putInt(curPos, 11);
             saveStream.putInt(curPos += 4, k.index);
             curPos += 8; // padSize is always 0x0
         }
-        if(curPos != 128)
+        if (curPos != 128)
             System.out.println("Keysets end at " + curPos + ". Should be 128.");
-        
-        
+
         try (FileOutputStream fos = new FileOutputStream("D:/SMGO/tools/CANM/saved.canm")) {
             fos.write(saveArray);
         }
@@ -186,7 +186,7 @@ public class CanmFile {
         file.setContents(saveArray);
         file.save();
     }
-    
+
     private FileBase file;
     private byte[] byteArray;
     private ByteBuffer byteStream;
